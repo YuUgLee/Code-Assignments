227613a60957c1634313dcf867d019ec3df2a775
YuUgLee <55852854+YuUgLee@users.noreply.github.com>
2019-10-15 18:26:31 -0700

    Brute Force Equation Solver
    Uses a certain range to solve a simple equation to test if the equation is true

---
+++ Brute Force Equation Solver.py
@@ -0,0 +1,17 @@
+''' Read in first equation, ax + by = c '''
+a = int(input())
+b = int(input())
+c = int(input())
+
+''' Read in second equation, dx + ey = f '''
+d = int(input())
+e = int(input())
+f = int(input())
+ans=0
+for x in range(-10,11):
+    for y in range(-10,11):
+        if (a*x+b*y==c) and (d*x+e*y==f):
+            print(x,y)
+            ans=1
+if ans==0:
+    print('No solution')
