9134917c04c93f1143ffe102b1ac24b4c93b69a5
YuUgLee <55852854+YuUgLee@users.noreply.github.com>
2019-10-28 20:45:09 -0700

    Experimental and Theoretical Inertia
    Calculates experimental and theoretical inertia based on measured inerta and no mass inertia.

---
+++ Experimental Inertia Calculator.py
@@ -0,0 +1,5 @@
+
+m=float(input('Enter measured inertia:'))
+o=float(input('Enter no-mass inertia:'))
+exp=m-o
+print('Experimental inertia is','%2E' %(exp))
---
+++ Theoretical Inertia Calculator.py
@@ -0,0 +1,4 @@
+r=float(input('Enter the radius:'))
+m=float(input('Enter the mass:'))
+the=(2*m)*(r**2)
+print('Theoretical Inertia is','%2E' %(the))
