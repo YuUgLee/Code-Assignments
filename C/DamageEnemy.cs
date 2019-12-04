9448af29abc36fbb0d85c7519c413efb3ff17224
YuUgLee <55852854+YuUgLee@users.noreply.github.com>
2019-12-04 13:43:17 -0800

    Enemy Damage
    Names the Enemy
    Sets Health to 100
    If enemy is damaged the current health is subtracted from specified number damage

---
+++ C/DamageEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEnemy : MonoBehaviour
+{
+    public string enemyName;
+    [SerializeField]
+    public float m_MaxHealth = 100;
+    public float m_CurrentHealth;
+    private void Awake()
+    {
+        m_CurrentHealth = m_MaxHealth;
+    }
+
+    public void Damage(float damage)
+    {
+        m_CurrentHealth -= damage;
+        if (Deadenemy())
+        {
+            Destroy(gameObject);
+        }
+    }
+    public bool Deadenemy() {return m_CurrentHealth <=0;}
+
+}
