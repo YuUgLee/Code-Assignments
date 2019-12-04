c6bde79e89d5723a8f669a0a4976ca38c852d748
YuUgLee <55852854+YuUgLee@users.noreply.github.com>
2019-12-04 13:42:29 -0800

    Damage Control
    Makes the Dash attack as a game object
    When Dash collides with the collderbox of Enemy, enemy will take damage and be subtracted from current health

---
+++ C/DamageControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageControl : MonoBehaviour
+{
+    [SerializeField] private float playerdashdamage;
+    [SerializeField] private DamageEnemy damagee;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Damage();
+        }
+    }
+    void Damage()
+    {
+        damagee.m_CurrentHealth -= playerdashdamage;
+        this.gameObject.SetActive(false);
+    }
+    
+}
