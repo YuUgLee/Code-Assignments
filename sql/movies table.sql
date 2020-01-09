04a17a8bebbf5b55e40d269070edb3d2b93914c3
YuUgLee <55852854+YuUgLee@users.noreply.github.com>
2020-01-09 12:41:11 -0800

    Update 1
    adding commnets for each action
    also additional action of changing the names of the columns

--- sql/movies table.sql
+++ sql/movies table.sql
@@ -1,23 +1,29 @@
-use world;
+use world;#must have a database to to make table in
 
-create table movies (
+create table movies ( #to create the table
 	name text,
     genre text,
     year integer,
     imdb_rating integer
     );
     
-select name, genre, year
+select name, genre, year #select * shows everything without having to type up all columns
+from movies; #like a print statement to show the table
+
+insert into movies (name, genre, year) #to insert values into table
+values ('Batman', 'action', 2008);
+
+insert into movies (name, genre, year)
+values ('Avengers: Endgame', 'action', 2018);
+
+insert into movies (name, genre, year)
+values ('Batman v. Superman: Dawn of Justice', 'action', 2016);
+
+select name as 'Titles' #changes the text display of the column
 from movies;
 
-insert into movies (name, genre, year)
-values ('Batman', action, 2008);
-
-insert into movies (name, genre, year)
-values ('Avengers: Endgame', action, 2018);
-
-insert into movies (name, genre, year)
-values ('Batman v. Superman: Dawn of Justice', action, 2016);
+select imdb_rating as 'IMDb'
+from movies;
 
     
     
